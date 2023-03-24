1a23e8c1f99f74790e34051f00f1cc2e795877f1
shayaan.siddiqui <shayaan.siddiqui@gmail.com>
2023-03-24 11:12:19 -0400

    updated design

---
+++ src/app/pipes/date.year.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateYearPipe } from './date.year.pipe';
+
+describe('DateYearPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateYearPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/date.year.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {DatePipe} from "@angular/common";
+
+@Pipe({
+  name: 'dateYear'
+})
+export class DateYearPipe extends DatePipe implements PipeTransform {
+
+  override transform(value: any, ...args: any): any {
+    return super.transform(value, "yyyy");
+  }
+
+}
