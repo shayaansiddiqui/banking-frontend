6fcfde41d5ce9701cf440c5840ed690164b5edd0
abdelopx <50838345+abdelopx@users.noreply.github.com>
2023-03-26 22:27:43 +0000

    fix routing and footer and ability to navigate to /transaction/{{id}}/accountType and get data on component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,18 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DepositComponent } from './pages/deposit/deposit.component';
 import { TransactionComponent } from './pages/transaction/transaction.component';
 
+
+//the path property should have names starting with a lowerCase (previously all names started with a upperCase character) (ex: Landing -> landing)
+//to be able to pass the params to a route just add /:nameOfYourVariable and you will be able to retrieve it using the ActivatedRouter (Please see transaction.component.ts to check on how to retrieve data from routes)
 const routes: Routes = [
-  {path: '', redirectTo: 'Landing', pathMatch: 'full'},
-  {path: 'registration', component: RegistrationComponent, data: {animation: 'isDown'}},
+  {path: '', redirectTo: 'landing', pathMatch: 'full'},
+  {path: 'registration',component: RegistrationComponent, data: {animation: 'isDown'}},
   {path: 'login', component: LoginComponent, data: {animation: 'isUp'}},
-  {path: 'Landing', component: LandingComponent},
-  {path: 'Header', component: HeaderComponent},
-  { path: 'Dashboard', component: DashboardComponent},
-  { path: 'Deposit', component: DepositComponent},
-  { path: 'Transaction', component: TransactionComponent}
+  {path: 'landing', component: LandingComponent},
+  {path: 'header', component: HeaderComponent},
+  { path: 'dashboard', pathMatch: 'full', component: DashboardComponent},
+  { path: 'deposit', component: DepositComponent},
+  { path: 'transaction/:id/:accountType', component: TransactionComponent}
 ];
 
 
--- src/app/pages/transaction/transaction.component.ts
+++ src/app/pages/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-transaction',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./transaction.component.css']
 })
 export class TransactionComponent {
+  //variables to store account and accountType.
+  id!: string;
+  accountType!: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  //Below is how to retrieve the id and account type from router and store them in variables
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')!;
+    this.accountType = this.route.snapshot.paramMap.get('accountType')!;
+    console.log("id -> ", this.id);
+    console.log("accountType -> ", this.accountType);
+  }
 
 }
