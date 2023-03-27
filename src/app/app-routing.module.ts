e57e4e95cf5a8115733930156a55d35014862a46
abdelopx <50838345+abdelopx@users.noreply.github.com>
2023-03-27 13:39:16 +0000

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import {RegistrationComponent} from "./pages/registration/registration.component
 import {LoginComponent} from "./pages/login/login.component";
 import {LandingComponent} from './pages/landing/landing.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {TransactionComponent} from "./pages/dashboard/transaction/transaction.component";
+import {SummaryComponent} from "./pages/dashboard/summary/summary.component";
+import {DepositComponent} from "./pages/dashboard/deposit/deposit.component";
 
 
 const routes: Routes = [
@@ -12,13 +15,20 @@ const routes: Routes = [
 	{path: 'registration', component: RegistrationComponent, data: {animation: 'isDown'}},
 	{path: 'login', component: LoginComponent, data: {animation: 'isUp'}},
 	{path: 'landing', component: LandingComponent},
-	{
-		path: 'business',
-		loadChildren: () => import('./pages/dashboard/dashboard.routing.module').then((m) => m.DashboardRoutingModule),
-		// canActivate: [AuthGuard],
-		// data: {roles: [Role.StandardUser, Role.StandardAdministrator, Role.StandardAdministrator]}
+	{path: 'dashboard', component: DashboardComponent, children: [
+			{
+				path: "transaction/:id/:accountType",
+				component: TransactionComponent
+			},
+			{
+				path: "summary/:id",
+				component: SummaryComponent,
+
+			},  {
+				path: "deposit",
+				component: DepositComponent,
+			}]
 	},
-	{path: 'dashboard', component: DashboardComponent},
 	{path: '', redirectTo: 'landing', pathMatch: "full"},
 
 ];
